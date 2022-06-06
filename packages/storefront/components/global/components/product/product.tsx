d26a9fa9189be251c5f36dcca00f1c3965ad6cec
AnikBS <anik.barua@brainstation-23.com>
2022-06-06 15:31:32 +0600

    added filtering

--- packages/storefront/components/global/components/product/product.tsx
+++ packages/storefront/components/global/components/product/product.tsx
@@ -3,35 +3,37 @@ import Picture from "./common/picture";
 import Icon from "../icon";
 
 const Product = (props: any) => {
-  const { product }: any = props;
+    const { product }: any = props;
 
-  return (
-    <>
-      <div className="mb-0 overflow-hidden" key={product.id}>
-        <div className="transition duration-0 hover:duration-700 group hover:bg-white cursor-pointer">
-          <div className="rounded overflow-hidden max-w-sm">
-            <div className="relative flex items-center justify-center flex-col">
-              <div className="relative text-white overflow-hidden transition-all duration-700">
-                <div className="relative inset-0 bg-cover bg-center z-0">
-                  <Picture
-                    product={product}
-                    height={200}
-                    width={200}
-                    src={product.images[0]}
-                    alt={product.category}
-                  />
+    return (
+        <>
+            <div className="mb-0 overflow-hidden" key={product.id}>
+                <div className="transition duration-0 hover:duration-700 group hover:bg-white cursor-pointer">
+                    <div className="rounded overflow-hidden max-w-sm">
+                        <div className="relative flex items-center justify-center flex-col">
+                            <div className="relative text-white overflow-hidden transition-all duration-700">
+                                <div className="relative inset-0 bg-cover bg-center z-0">
+                                    <Picture
+                                        product={product}
+                                        height={200}
+                                        width={200}
+                                        src={product.images[0]}
+                                        alt={product.category}
+                                    />
+                                </div>
+                            </div>
+                            <div className="hover:-translate-y-3 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-black font-semibold">
+                                <Icon />
+                            </div>
+                            <div className="text-center">
+                                <ProductInfo product={product}></ProductInfo>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="hover:-translate-y-3 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-black font-semibold">
-                <Icon />
-              </div>
-              <ProductInfo product={product} />
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Product;
--- packages/storefront/components/trend/index.tsx
+++ packages/storefront/components/trend/index.tsx
@@ -1,35 +1,83 @@
 import { FC } from "react";
+
 import productData from "../../allData/product-data.json";
 import Product from "../global/components/product/product";
 import SwiperGrid from "../global/components/swipergrid";
 import { SwiperSlide } from "swiper/react";
 import Container from "../global/components/container";
+import { useEffect, useState } from "react";
 
 const TrendingProducts: FC = () => {
-  return (
-    <>
-      <Container>
-        <div className="text-center mb-6">
-          <p className="text-lg font-serif italic">Recently added our store</p>
-          <h1 className="text-4xl text-bold ">Trending Products</h1>
-        </div>
-        <SwiperGrid
-          slidesPerViewmobile={2}
-          slidesPerView768={3}
-          slidesPerView980={5}
-          rows={2}
-        >
-          {productData["products"] &&
-            productData["products"].length > 0 &&
-            productData.products.map((product: any) => (
-              <SwiperSlide key={product.id}>
-                <Product product={product} />
-              </SwiperSlide>
-            ))}
-        </SwiperGrid>
-      </Container>
-    </>
-  );
+    let [filterKey, setFilterKey]: any = useState("smartphones");
+    let [filteredProduct, setProducts]: any = useState();
+
+    useEffect(() => {
+        const newProduct = productData.products.filter(
+            (product: any) => product.category === "smartphones"
+        );
+        setProducts(newProduct);
+    }, []);
+
+    const handleClick = (text: any) => {
+        const newProduct = productData.products.filter(
+            (product: any) => product.category === text
+        );
+        setProducts(newProduct);
+        setFilterKey(text)
+    };
+    return (
+        <>
+            <Container>
+                <div className="text-center mb-6">
+                    <p className="text-lg font-serif italic">
+                        Recently added our store
+                    </p>
+                    <h1 className="text-4xl text-bold ">Trending Products</h1>
+                </div>
+
+                <ul className="flex justify-center text-xl m-5 font-semibold ">
+                    <li className="mr-3">
+                        <button
+                            onClick={() => handleClick("smartphones")}
+                            className = {filterKey === "smartphones" ? "border border-[#40A944] inline-block rounded-full bg-white text-[#40A944] px-4 py-1": "inline-block rounded-full bg-white text-black px-4 py-1"}
+                        >
+                            SMARTPHONES
+                        </button>
+                    </li>
+                    <li className="mr-3">
+                        <button
+                            onClick={() => handleClick("laptops")}
+                            className = {filterKey === "laptops" ? "border border-[#40A944] inline-block rounded-full bg-white text-[#40A944] px-4 py-1": "inline-block rounded-full bg-white text-black px-4 py-1"}
+                        >
+                            LAPTOPS
+                        </button>
+                    </li>
+                    <li className="mr-3">
+                        <button
+                            onClick={() => handleClick("fragrances")}
+                            className = {filterKey === "fragrances" ? "border border-[#40A944] inline-block rounded-full bg-white text-[#40A944] px-4 py-1": "inline-block rounded-full bg-white text-black px-4 py-1"}
+                        >
+                            FRAGRANCES
+                        </button>
+                    </li>
+                </ul>
+
+                <SwiperGrid
+                    slidesPerViewmobile={2}
+                    slidesPerView768={3}
+                    slidesPerView980={5}
+                    rows={2}
+                >
+                    {filteredProduct?.length > 0 &&
+                        filteredProduct?.map((product: any) => (
+                            <SwiperSlide key={product.id}>
+                                <Product product={product} />
+                            </SwiperSlide>
+                        ))}
+                </SwiperGrid>
+            </Container>
+        </>
+    );
 };
 
 export default TrendingProducts;
