c89186a3dc0c9a19bc909661588ef4f8f4a4c215
AnikBS <anik.barua@brainstation-23.com>
2022-06-07 11:25:14 +0600

    fixed left and right portion

--- packages/storefront/components/checkout/shippingPage/shipping.tsx
+++ packages/storefront/components/checkout/shippingPage/shipping.tsx
@@ -11,14 +11,8 @@ const path = {
 
 const  Shipping = () => {
     return (
-        <div className="w-3/5">
-            <div className="px-3.5 m-0 lg:mx-20 lg:my-12">
-                {/* <Path
-                    cart={path.cart}
-                    info={path.info}
-                    shipping={path.shipping}
-                    payment={path.payment}
-                ></Path> */}
+        <div className="w-full sm:w-full md:w-full lg:w-3/5 xl:w-3/5">
+            <div className="px-3.5 m-0 lg:mx-20 lg:my-10">
                 <div className="box-border p-4 border-2 mt-10 divide-y-2 rounded-md text-center mx-auto">
                     <div className="flex justify-between items-center p-4">
                         <div className="flex flex-wrap gap-4">
