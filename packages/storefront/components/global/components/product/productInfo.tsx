d7ec332f7c72ec2b1ef363503900d9836a706641
AnikBS <anik.barua@brainstation-23.com>
2022-06-07 13:43:30 +0600

    modified productInfo

--- packages/storefront/components/global/components/product/productInfo.tsx
+++ packages/storefront/components/global/components/product/productInfo.tsx
@@ -2,22 +2,32 @@ import React from "react";
 import Icon from "../icon";
 
 const ProductInfo = (props: any) => {
-  const { product }: any = props;
-  return (
-    <div>
-      <div className="px-6">
-        <div className="text-base font-medium text-gray-600">
-          {product.title}
+    const { product }: any = props;
+    return (
+        <div>
+            <div className="px-6">
+                <div className="text-base font-medium text-gray-600">
+                    {product.title}
+                </div>
+                <p className="text-xs font-['arial'] text-gray-600 py-2">
+                    {product.category}
+                </p>
+                <div className="text-lg font-semibold text-green-600">
+                    ${product.price}
+                    {product.prevPrice ? (
+                        <span className="text-lg font-semibold text-black ml-2">
+                            <s>$</s>
+                            {product.prevPrice ? (
+                                <s>{product.prevPrice}</s>
+                            ) : null}
+                        </span>
+                    ) : (
+                        <div></div>
+                    )}
+                </div>
+            </div>
         </div>
-        <p className="text-xs font-['arial'] text-gray-600 py-2">
-          {product.category}
-        </p>
-        <p className="text-base font-semibold text-green-600">
-          {product.price}
-        </p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProductInfo;
