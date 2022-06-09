6b7959faeb3d7876a7212ecbfe8b08c9144ed215
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-09 12:22:01 +0600

    API

--- packages/storefront/pages/_app.tsx
+++ packages/storefront/pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import "@/styles/globals.css";
 
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
